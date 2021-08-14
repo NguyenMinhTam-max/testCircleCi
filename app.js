4c087d75a49c0a83c3fc34383a1a7abb513740ce
NguyenMinhTam-max <nguyenminhtam1106999@gmail.com>
2021-08-15 03:44:52 +0700

    test

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const environment = require('./environments/environment')
 const server = require('./routersConfig/server')
 const routers = require('./routersConfig/routers')
-const validator = require('../testCircleCi/validator')
 server.set('port', environment.portServer)
 server.use(routers)
 
 
 server.listen(environment.portServer, environment.ipServer, () => {
-	console.log(`Server Is Starting At: ${environment.ipServer}:${environment.portServer}`)
+	console.log(`Server Is Starting At:${environment.portServer}`)
 })
 console.log('testing purpose')
