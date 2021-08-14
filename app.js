c2be0b9a997051bbe0cfc06a7a7ca7053d67a425
NguyenMinhTam-max <nguyenminhtam1106999@gmail.com>
2021-08-15 03:23:47 +0700

    testing

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const routers = require('./routersConfig/routers')
 const validator = require('../testCircleCi/validator')
 server.set('port', environment.portServer)
 server.use(routers)
-console.log(validator.isNumValid(39))
+
 
 server.listen(environment.portServer, environment.ipServer, () => {
 	console.log(`Server Is Starting At: ${environment.ipServer}:${environment.portServer}`)
 })
+console.log('testing')
